d95152260f1c52a6490591b961dc68eecf12f230
CODINGMASTER197 <100692426+CODINGMASTER197@users.noreply.github.com>
2023-01-31 18:29:29 +0530

    Add files via upload

---
+++ bottles.js
@@ -0,0 +1,26 @@
+status = "";
+objects = [];
+
+function preload(){
+    img = loadImage("bottles.jpg");
+}
+
+function setup(){
+    canvas = createCanvas(380,380);
+    canvas.center();
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status : Detecting Objects";
+}
+
+function modelLoaded(){
+    console.log("Model Loaded!");
+    status = true;
+}
+
+function gotresults(){
+    if(error){
+        console.error(error);
+    }
+    console.log(results);
+    objects = results;
+}
